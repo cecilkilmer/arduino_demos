d4c81cccbebd980925c7e5d0987a190c19076112
Matt Dissinger <matt.dissinger@gmail.com>
2015-02-24 19:30:24 -0600

    Update blink sample to read from serial new on,off values.

--- sketch_02_blink/sketch_02_blink.ino
+++ sketch_02_blink/sketch_02_blink.ino
@@ -1,14 +1,47 @@
-int ledPin = 13;
+const int nLEDPin = 13;
+
+// Adjustable values
+int nOnTimeMS = 50;
+int nOffTimeMS = 450;
+int nLEDState = HIGH;
+long lPrevMills = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(nLEDPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, HIGH);
-  delay(50);
-  digitalWrite(ledPin, LOW);
-  delay(450);
+  unsigned long lCurMills = millis();
+  
+  if (nLEDState == HIGH && lCurMills - lPrevMills > nOnTimeMS)
+  {
+    nLEDState = LOW;
+    lPrevMills = lCurMills;
+  }
+  else if (nLEDState == LOW && lCurMills - lPrevMills > nOffTimeMS)
+  {
+    nLEDState = HIGH;
+    lPrevMills = lCurMills;
+  }
+
+  digitalWrite(nLEDPin, nLEDState);
+  
+  while (Serial.available() > 0)
+  {
+    int nNewOnTime = Serial.parseInt();
+    int nNewOffTime = Serial.parseInt();
+    
+    if (Serial.read() == '\n')
+    {
+      nOnTimeMS = constrain(nNewOnTime, 25, 5000);
+      nOffTimeMS = constrain(nNewOffTime, 25, 5000);
+
+      Serial.println("Set values:");
+      Serial.println(nOnTimeMS);
+      Serial.println(nOffTimeMS);
+    }
+  }
 }
